67b8ee74e75b50c5edd958a2a5a5147c7008073d
Алексей Никулин <alexey_nik_ural@mail.ru>
2024-04-15 13:20:04 +0500

    убрали ожидание

--- SeleniumTestForPractics/SeleniumTest.cs
+++ SeleniumTestForPractics/SeleniumTest.cs
@@ -11,17 +11,22 @@ public class SeleniumTest
     {
         var options = new ChromeOptions();
         options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        
         var driver = new ChromeDriver(options);
         driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
         Thread.Sleep(5000);
+        
         var login = driver.FindElement(By.Id("Username"));
         login.SendKeys("alexey_nik_ural@mail.ru");
+        
         var password = driver.FindElement(By.Name("Password"));
         password.SendKeys(".AlexeyTitan2001.");
         Thread.Sleep(3000);
+        
         var enter = driver.FindElement(By.Name("button"));
         enter.Click();
         Thread.Sleep(3000);
+        
         var currentUrl = driver.Url;
         Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
         driver.Quit();
